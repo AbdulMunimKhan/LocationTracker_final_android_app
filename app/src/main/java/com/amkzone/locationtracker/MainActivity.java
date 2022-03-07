9e866502144306105d8f30f470c25d9a0af94891
unknown <munim.ctl@gmail.com>
2022-03-07 07:28:10 +0500

    second  commit

--- app/src/main/java/com/amkzone/locationtracker/MainActivity.java
+++ app/src/main/java/com/amkzone/locationtracker/MainActivity.java
@@ -1,15 +1,19 @@
 package com.amkzone.locationtracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
@@ -20,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int UPDATE_INTERVAL = 30;
     public static final int FAST_INTERVAL = 50;
-    private TextView tv_lat, tv_lon, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
+    private TextView tv_lat, tv_lon, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address,tv_altitude;
     private Switch sw_locationupdates, sw_gps;
 
     FusedLocationProviderClient fusedLocationProviderClient;
@@ -38,11 +43,12 @@ public class MainActivity extends AppCompatActivity {
         tv_address = findViewById(R.id.tv_address);
         tv_sensor = findViewById(R.id.tv_sensor);
         tv_updates = findViewById(R.id.tv_update);
+        tv_altitude = findViewById(R.id.tv_alti);
 
         sw_locationupdates = findViewById(R.id.sw_locationupdates);
         sw_gps = findViewById(R.id.sw_gps);
 
-        locationRequest = new LocationRequest();
+        locationRequest = LocationRequest.create();
         locationRequest.setInterval(1000 * UPDATE_INTERVAL);
         locationRequest.setFastestInterval(1000 * FAST_INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
@@ -59,19 +65,63 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+        //The function Call
+        updateGps();
 
     }
-    private void updateGps(){
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGps();
+                }else{
+                    Toast.makeText(this, "This app requires Permission", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;  
+
+        }
+    }
+
+    private void updateGps(){
+        Log.d("Inside the update Gps","Start");
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        Log.d("After Fused","It is after the fusedlocationproviderclient statement... amkhan");
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.d("Manifest", "Successfully manifest executed.. Abdul Munim Khan");
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
                     /// we get persmission
+                    updateUIValues(location);
                 }
             });
+        }else{
+            //permission not granted
+            if(Build.VERSION.SDK_INT == Build.VERSION_CODES.M){
+                Log.d("Manifest", "there erro in the manifest.. Abdul Munim Khan");
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private void updateUIValues(Location location) {
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+        if(location.hasAltitude()){
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }else{
+            tv_altitude.setText("Not Available");
+        }
+        if(location.hasSpeed()){
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }else{
+            tv_speed.setText("Not Available");
         }
     }
 }
