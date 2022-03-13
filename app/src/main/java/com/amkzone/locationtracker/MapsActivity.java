ea0853629d8ec67e575aafb1cf2ebe4db6d17a1d
unknown <munim.ctl@gmail.com>
2022-03-13 10:19:07 +0500

    initial commit

--- app/src/main/java/com/amkzone/locationtracker/MapsActivity.java
+++ app/src/main/java/com/amkzone/locationtracker/MapsActivity.java
@@ -1,22 +1,28 @@
 package com.amkzone.locationtracker;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.amkzone.locationtracker.databinding.ActivityMapsBinding;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    private List<Location> savedLocations;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +34,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        MyApplication myApplication = (MyApplication) getApplicationContext();
+        savedLocations = myApplication.getMyLocations();
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -47,5 +55,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng lastLocationSaved = sydney;
+        for(Location location:savedLocations){
+            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Lat: " + location.getLatitude() + " Long: " + location.getLongitude());
+            mMap.addMarker(markerOptions);
+            lastLocationSaved = latLng;
+
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocationSaved,12.0f));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+                Integer clicks = (Integer) marker.getTag();
+                if(clicks == null){
+                    clicks = 0;
+                }
+                    clicks++;
+                marker.setTag(clicks);
+
+                Toast.makeText(MapsActivity.this, "Marker " + marker.getTitle() + " has " + marker.getTag() + " clicks", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
